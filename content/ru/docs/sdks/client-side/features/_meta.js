269ab04586bf19a1040a7f777661874a42fe94a2
Dmitrii Zolotukhin <118734134+dmitry-simple-it@users.noreply.github.com>
2026-09-02 17:29:12 +0300

    docs(sdk): промокоды и офферы на клиенте

--- content/ru/docs/sdks/client-side/features/_meta.js
+++ content/ru/docs/sdks/client-side/features/_meta.js
@@ -2,5 +2,6 @@ export default {
   'click-tracking': 'Отслеживание кликов',
   'conversion-tracking': 'Отслеживание конверсий',
   'cross-domain-tracking': 'Кросс‑доменный трекинг',
+  'promo-codes': 'Промокоды и офферы',
   'reverse-proxy-support': 'Reverse proxy',
 }
